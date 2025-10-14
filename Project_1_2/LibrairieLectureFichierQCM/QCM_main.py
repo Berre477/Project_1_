ce76529e676b6674001f309439a7a887d0a5ce13
Berre47 <alexander.home@student.uclouvain.be>
2025-10-14 21:07:24 +0200

    QCM marche il faut just le commenter et le clean

--- Project_1_2/LibrairieLectureFichierQCM/QCM_main.py
+++ Project_1_2/LibrairieLectureFichierQCM/QCM_main.py
@@ -27,9 +27,23 @@ translate_letter_to_number={"A":"0","B":"1","C":"2","D":"3","E":"4"}
 translate_letter_to_number=str.maketrans(translate_letter_to_number)
 
 def QCM(questions: list):
-    Grade=0
+    right_answers=0
+    wrong_answers=0
     over=len(questions)
 
+
+    while True:
+        print("Choisissez votre mode de cotation : \nA: Légère : +1 pour les bonnes réponses ,0 pour les mauvaises \nB: sévère : +1 pour les bonnes réponses ,-1 pour les mauvaises")
+        quoting=input("Choisisez A ou B : ").lower()
+        if quoting in ["a","b"]:
+            quoting_value=True if quoting == "a" else False
+            break
+        else:
+            print("S'il vous plait choisissez A ou B")
+    print("Choisissez votre mode de présentation :\nA: Evaluation : un seul résultat pour un mode de résultat choisis\nB: Comparatif: résultats pour les 2 modes de cotation  " )
+    presentation=input("Choisissez A ou B : ").lower()
+    if presentation in ["a","b"]:
+        presentation_value=True if presentation == "a"else False
 
     for i in range(len(questions)):
         question_num = PRNG(len(questions))
@@ -41,11 +55,8 @@ def QCM(questions: list):
             ans=answers[answer_num]
             answers.remove(ans)
             random_answers.append(ans)
-
         answer_list={}
-
         print(f"Q:{question[0]}")
-        letter_list=[]
         tried=True
         while tried:
             try:
@@ -56,21 +67,51 @@ def QCM(questions: list):
 
 
                 answer = input(f"answer : ").upper()
-                answer=int(answer.translate(translate_letter_to_number))
+                answer =list(answer)
+                for t in answer:
+                    if t == ' ':
+                        answer.remove(t)
+
+                answer_to_number=[]
+                for u in answer:
+                    z=u.translate(translate_letter_to_number)
+                    answer_to_number.append(int(z))
 
 
-                if answer_list[answer]:
+                Good_answers={}
+                Bad_answers={}
+                for i in range(len(answer_list)):
+                    if answer_list[i] :
+                        Good_answers[i] = answer_list[i]
+                    else:
+                        Bad_answers[i] = answer_list[i]
+
+                if answer_to_number == list(Good_answers.keys()):
                         print("right answer\n")
                         tried=False
-                        Grade+=1
+                        right_answers+=1
                 else:
                         print("wrong answer\n")
                         tried=False
+                        wrong_answers+=1
 
                 questions.remove(questions[question_num])
             except ValueError:
                 pass
-    print(f"{Grade}/{over}")
+    light_quoting=f"{right_answers}/{over}"
+    severe_quoting=f"{right_answers-wrong_answers}/{over}"
+
+    if presentation_value:#Evaluation
+        if quoting_value:
+            print("Résultat leger: "+light_quoting)
+        else:
+            print("Résultat severe: "+severe_quoting)
+    else:#comparatif
+        print(f"Résultat leger: {light_quoting}  \t Résultat severe: {severe_quoting}")
+
+
+
+
 
 QCM(full_question)
 
